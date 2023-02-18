16af756d5af26fe0d1204b974a65fb940d9e6745
MarjitaSaha <marjitasaha@gmail.com>
2023-02-18 18:01:04 +0530

    Contest-2 1st commit

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -5,6 +5,7 @@ public class Email {
     private String emailId;
     private String password;
 
+
     public Email(String emailId){
         this.emailId = emailId;
         this.password = "Accio@123";
@@ -18,6 +19,22 @@ public class Email {
         return password;
     }
 
+    public boolean check(String password)
+    {
+       if(password.length()<8) return false;
+        boolean upper=false;
+        boolean lower=false;
+        boolean digit=false;
+        boolean special=false;
+        for (int i=0;i<password.length();i++)
+        {
+            if(password.charAt(i)>='A' && password.charAt(i)<='Z') upper=true;
+            if(password.charAt(i)>='a' && password.charAt(i)<='z') lower=true;
+            if(password.charAt(i)>='1' && password.charAt(i)<='9') digit=true;
+            if(password.charAt(i)<='A' && password.charAt(i)>='Z' && password.charAt(i)<='a' && password.charAt(i)>='z' && password.charAt(i)<='1' && password.charAt(i)>='9') special=true;
+        }
+        return upper && lower && digit && special;
+    }
     public void changePassword(String oldPassword, String newPassword){
         //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
         // 1. It contains at least 8 characters
@@ -25,5 +42,12 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if(oldPassword.equals(password))
+        {
+            if (check(newPassword))
+            {
+                password=newPassword;
+            }
+        }
     }
 }
